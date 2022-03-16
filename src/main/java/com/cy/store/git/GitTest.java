bc0295621136dc87c9dec446ff5adf5194bf9130
Jcliu <2374072109@qq.com>
2022-03-16 15:09:13 +0800

    second

--- src/main/java/com/cy/store/git/GitTest.java
+++ src/main/java/com/cy/store/git/GitTest.java
@@ -7,5 +7,6 @@ package com.cy.store.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello Super!");
     }
 }
