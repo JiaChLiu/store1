75c663988b85c045437129a07de1aed861b2e429
Jcliu <2374072109@qq.com>
2022-03-16 15:04:25 +0800

    first

---
+++ src/main/java/com/cy/store/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cy.store.git;
+
+/**
+ * @author Ljcstart
+ * @create 2022-03-16 15:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
